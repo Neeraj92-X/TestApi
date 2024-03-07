422f242dd48d05f1034ac4eb45731e2bf5424be5
Neeraj92-X <51928834+Neeraj92-X@users.noreply.github.com>
2024-03-07 11:06:35 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const userController = require("../src/controllers/user.controller");
 
 module.exports = (router) => {
-  router.get("/testApi", userController.testApi);
+  router.get("/test", userController.testApi);
 };
